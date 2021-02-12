38c7b0e5461c936dfed1f11da60a3dd8f9dc74bf
Chinapanda <kittikornchina2014@gmail.com>
2021-02-13 02:54:39 +0700

    pj

---
+++ PJ02 MARK/src/User.java
@@ -0,0 +1,21 @@
+// Name:sarun junbang
+// Student ID:6188080
+// Section: 2
+
+public class User {
+	public int uid;
+	
+	public User(int _id){
+		
+		// YOUR CODE GOES HERE     //conductor
+		this.uid=_id;
+	}
+	
+	public int getID(){
+		
+		// YOUR CODE GOES HERE
+		
+		return uid;
+	}
+
+}
