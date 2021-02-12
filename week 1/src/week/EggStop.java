d407bb80cecbde78937f1fbfad11db760d02965f
Chinapanda <kittikornchina2014@gmail.com>
2021-02-13 02:55:07 +0700

    week 1

---
+++ week 1/src/week/EggStop.java
@@ -0,0 +1,26 @@
+package week;
+
+public class EggStop {
+	public static void excecute() {
+		System.out.println("  ________");
+		System.out.println(" /        \\");
+		System.out.println("/          \\");
+		System.out.println("\\          /");
+		System.out.println(" \\________/\n");
+		
+	}
+	public static void main(String[] agrs) {
+		excecute();
+		excecute();
+		System.out.println("+----------+");
+		System.out.println("  \n\n  ________");
+		System.out.println(" /        \\");
+		System.out.println("/          \\");
+		System.out.println("|   STOP   |");
+		System.out.println("\\          /");
+		System.out.println(" \\________/");
+		System.out.println("+----------+");
+	
+	}
+
+}
